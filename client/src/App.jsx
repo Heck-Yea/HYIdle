9d6b2216dbe26fb66d6f98cd58b299df8f5d9696
Heck-Yea <144502918+Heck-Yea@users.noreply.github.com>
2025-04-27 11:25:26 -0600

    try to get user creation to work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import UserForm from './components/UserForm';
+import Record from './components/Record';
 
-const App = () => {
+function App() {
+  const [currentTab, setCurrentTab] = useState('mining');
+
   return (
-    <div className="w-full p-6">
-      <Navbar />
-      <Outlet />
+    <div>
+      <nav>
+        <button onClick={() => setCurrentTab('Navbar')}>Navbar</button>
+        <button onClick={() => setCurrentTab('UserForm')}>UserForm</button>
+        <button onClick={() => setCurrentTab('Record')}>Record</button>      
+      </nav>
+
+      <div>
+        {currentTab === 'Navbar' && <Navbar />}
+        {currentTab === 'UserForm' && <UserForm />}
+        {currentTab === 'Record' && <Record />}
+      </div>
     </div>
   );
-};
-export default App
+}
+
+export default App;
